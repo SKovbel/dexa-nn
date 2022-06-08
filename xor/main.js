ae80234e407dae38595a64a62b7941683d333037
SKovbel <skovbel@lesite.ca>
2022-06-08 09:38:18 +0200

    xor refactor

--- xor/main.js
+++ xor/main.js
@@ -4,8 +4,8 @@ class Main {
         {inputs: [0,0,1], outputs: [1], train: false},
         {inputs: [0,1,0], outputs: [1], train: true},
         {inputs: [0,1,1], outputs: [0], train: true},
-        {inputs: [1,0,0], outputs: [1], train: false},
-        {inputs: [1,0,1], outputs: [0], train: true},
+        {inputs: [1,0,0], outputs: [1], train: true},
+        {inputs: [1,0,1], outputs: [0], train: false},
         {inputs: [1,1,0], outputs: [0], train: true},
         {inputs: [1,1,1], outputs: [1], train: true},
     ];
@@ -13,18 +13,18 @@ class Main {
     constructor(contentElement, networkCanvas) {
         this.contentElement = contentElement;
         this.networkCanvas = networkCanvas;
-    }
 
-    start() {
-        const network = new NeuralNetwork([
-            {size: 3, activation: NeuralNetworkActivation.RELU},
+        this.network = new NeuralNetwork([
             {size: 3, activation: NeuralNetworkActivation.TANH},
+            {size: 3, activation: NeuralNetworkActivation.RELU},
             {size: 3, activation: NeuralNetworkActivation.SOFTMAX},
             {size: 3, activation: NeuralNetworkActivation.SIGMOID},
             {size: 1},
         ]);
+    }
 
-        // prepare train
+    train() {
+        // prepare train, skip some results
         let newTrains = [];
         for (let i = 0; i < this.trains.length ; i++) {
             if (this.trains[i].train) {
@@ -33,22 +33,25 @@ class Main {
         }
 
         // train
-        NeuralNetworkBackPropagation.train(network, NeuralNetworkBackPropagation.SGD, newTrains, 0.1, 0.000001, 1000000);
+        NeuralNetworkBackPropagation.train(this.network, NeuralNetworkBackPropagation.SGD, newTrains, 0.1, 0.001, 1000000);
+    }
 
-        // check
+    test() {
         for (let t = 0; t < this.trains.length; t++) {
-            const result = NeuralNetwork.feedforward(network, this.trains[t].inputs);
-
+            const result = NeuralNetwork.feedforward(this.network, this.trains[t].inputs);
+            let ins = '';
+            for (let i = 0; i < this.trains[t].inputs.length; i++) {
+                ins += this.trains[t].inputs[i] + ' ';
+            }
+            let out = '';
+            for (let i = 0; i < this.trains[t].outputs.length; i++) {
+                out += this.trains[t].outputs[i] + ' ';
+            }
             const msg = document.createElement('p');
-            msg.innerText = '(' + (this.trains[t].train ? 'train' : 'skip') + ') ' + result;
+            msg.innerText = '(' + (this.trains[t].train ? 'train' : 'skip ') + ') ' + 
+                parseFloat(result).toFixed(4) + '   [ ' + ins + '] => [ ' + out + ']';
             msg.style.color = Math.round(result) == this.trains[t].outputs[0] ? 'green' : 'red';
             this.contentElement.append(msg);
         }
     }
 }
-
-const main = new Main(
-    document.getElementById('content'),
-    document.getElementById('network-canvas')
-);
-main.start();
